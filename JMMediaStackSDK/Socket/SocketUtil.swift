57a35e5a3b12a48297a58cb26fcdb533cbc53dff
Harsh Surati <harsh1.surati@ril.com>
2023-08-28 11:14:09 +0530

    Socket file structured

--- JMMediaStackSDK/Socket/SocketUtil.swift
+++ JMMediaStackSDK/Socket/SocketUtil.swift
@@ -7,6 +7,76 @@
 
 import Foundation
 
+enum SocketDataKey: String {
+    case producerId
+    case producerPeerId
+    case peerId
+    case mediaType
+    case video
+    case audio
+    case appData
+    case rtpParameters
+    case kind
+    case status
+    case data
+    case consumerInfo
+    case ok
+    case consumerId
+    case transportId
+    case dtlsParameters
+    case device
+    case rtpCapabilities
+    case sctpCapabilities
+    case metaData
+    case userType
+    case userRole
+    case human
+    case host
+    case videoGoogleStartBitrate
+    case receiveTransport
+    case sendTransport
+    case iceParameters
+    case iceCandidates
+}
+
+enum ReceiveTransportKey: String {
+    case dtlsParameters
+    case iceServers
+    case iceTransportPolicy
+}
+
+class JioSocketProperty {
+    static func getCloseProducerProperty(producerId: String) -> [String: Any] {
+        return [
+            "producerId": producerId
+        ]
+    }
+    
+    static func getClosePeerLeaveProperty(peerId: String) -> [String: Any] {
+        return [
+            "peerId": peerId
+        ]
+    }
+    
+    static func getConsumeProperty(producerId: String) -> [String: Any] {
+        return [
+            "producerId": producerId
+        ]
+    }
+    
+    static func getResumeConsumerProperty(consumerId: String) -> [String: Any] {
+        return [
+            "consumerId": consumerId
+        ]
+    }
+    
+    static func getPauseAndResumerProducerProperty(producerId: String) -> [String: Any] {
+        return [
+            "producerId": producerId
+        ]
+    }
+}
+
 struct SocketUtil {
     public static func getSocketKey() -> Int {
         var randomString = ""
