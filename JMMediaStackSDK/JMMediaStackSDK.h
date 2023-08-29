997661f291352c6de00c65692564fa258f5dd37a
Harsh Surati <harsh1.surati@ril.com>
2023-08-29 12:41:46 +0530

    Documents File updation

--- JMMediaStackSDK/JMMediaStackSDK.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <JMMediaStackSDK.h>
-
-FOUNDATION_EXPORT double JMMediaStackSDKVersionNumber;
-FOUNDATION_EXPORT const unsigned char JMMediaStackSDKVersionString[];
