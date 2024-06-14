836a4aa8f005c64b567a2df7633347957dbaa1a5
Onkar <Onkar.dhanlobhe@ril.com>
2024-06-14 14:20:52 +0530

    fix reconnection stuck issue for callkit call issue

--- JMMediaSampleApp/JMMediaStackSDK/Socket/JioSocket.swift
+++ JMMediaSampleApp/JMMediaStackSDK/Socket/JioSocket.swift
@@ -86,6 +86,7 @@ class JioSocket : NSObject {
     var callObserver: CXCallObserver!
     
     var selfPeerId: String = ""
+    var reconnectAttemptNO = -1
     
     private enum SocketKey: String {
         case roomId
@@ -116,6 +117,7 @@ class JioSocket : NSObject {
                 socket?.connect(withPayload: getPayload(roomId: roomId, jwtToken: jwtToken, isRejoin: isRejoin))
             }
         }
+        reconnectAttemptNO = -1
     }
     
     func disconnectSocket() {
@@ -200,7 +202,9 @@ extension JioSocket {
             .reconnectAttempts(10), // Set the number of reconnection attempts
             .reconnectWait(1),
             .reconnectWaitMax(2),//After 2 secs, it will attempt to reconnect //Ping pong takes 8 seconds to detect network loss. Total = 20sec.
-            .forceWebsockets(true)
+            .forceWebsockets(true),
+            .forcePolling(true),
+            .extraHeaders(["pingInterval": "5000", "pingTimeout": "10000"])
         ]
     }
     
@@ -241,6 +245,16 @@ extension JioSocket {
             else if event == .reconnectAttempt  {
                 socket?.on(clientEvent: .reconnectAttempt) { data, ack in
                     LOG.debug("Reconnect- reconnectAttempting... | \(data.description)")
+                    if let reconnectAttemptNo = self.convertToInt(data: data) {
+                        if self.reconnectAttemptNO != -1 && self.reconnectAttemptNO == reconnectAttemptNo {
+                            self.delegate?.didConnectionStateChange(.disconnected)
+                            self.reconnectAttemptNO = -1
+                        }
+                        self.reconnectAttemptNO = reconnectAttemptNo
+                        print("Reconnect- Converted number: \(reconnectAttemptNo)")
+                    } else {
+                        print("Reconnect- Conversion failed")
+                    }
                     self.delegate?.didConnectionStateChange(.reconnecting)
                 }
             }
@@ -251,6 +265,33 @@ extension JioSocket {
             }
         }
     }
+    
+        func convertToInt(data: Any?) -> Int? {
+            if let number = data as? Int {
+                return number
+            } else if let string = data as? String, let number = Int(string) {
+                return number
+            } else if let double = data as? Double {
+                return Int(double)
+            } else if let float = data as? Float {
+                return Int(float)
+            } else if let array = data as? [Any] {
+                // Optional: Handle array, e.g., take the first element and convert it
+                if let firstElement = array.first {
+                    return convertToInt(data: firstElement)
+                } else {
+                    print("Array is empty, cannot convert to Int")
+                }
+            } else {
+                if let data = data {
+                    print("Cannot convert data of type \(type(of: data)) to Int")
+                } else {
+                    print("Data is nil")
+                }
+                return nil
+            }
+            return nil
+        }
 }
 
 extension JioSocket:CXCallObserverDelegate {
