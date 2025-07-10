4d6d0697ae3b941eb71534ab0611631ba8372311
Onkar Dhanlobhe <134289491+NSCodeRover@users.noreply.github.com>
2025-07-10 17:20:43 +0530

    Update Package.swift ios 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LiveKit",
     platforms: [
-        .iOS(.v17),
+        .iOS(.v13),
         .macOS(.v10_15),
         .macCatalyst(.v14),
     ],
